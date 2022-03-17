6eb683ed496aeccc3a87a6a70d240b7e592e3bcd
Walter Heredia <walter.heredia@napse.global>
2022-03-17 17:26:38 -0300

    adding morgan

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, {Application} from 'express';
+import morgan from 'morgan';
 import {routes} from './routes';
 import dotenv from 'dotenv';
 import {sayHello} from "./utils";
 dotenv.config();
 
 const app: Application = express();
+app.use(morgan('dev'));
 app.use('/karvi', routes)
+
 
 app.listen(process.env.PORT || 1000, () =>
     {
